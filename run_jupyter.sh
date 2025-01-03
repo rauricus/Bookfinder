4cf0e5c378cbb853cfd80e090f8274f718b2479e
Andreas Weder <andreas@weder.net>
2025-01-03 15:29:55 +0100

    Ensure micromamba initialisation is the same everywhere.

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -11,20 +11,17 @@ NOTEBOOK_DIR="$HOME/Documents/Projekte/Objekterkennung.$CONDA_ENV/notebooks"
 # Note that we currently DO NOT initialize homebrew here as fallback after conda as well.
 
 # --- Initialize micromamba
-#     The following code is copied from "micromamba shell init"
-export MAMBA_EXE="/opt/homebrew/bin/micromamba";
-export MAMBA_ROOT_PREFIX="/Users/andreas/micromamba";
-__mamba_setup="$('/opt/homebrew/bin/micromamba' shell hook --shell zsh --prefix '/Users/andreas/micromamba' 2> /dev/null)"
+# 	  The following code is copied from "micromamba shell init"
+export MAMBA_EXE='/opt/homebrew/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/Users/andreas/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
 else
-    if [ -f "/Users/andreas/micromamba/etc/profile.d/micromamba.sh" ]; then
-        . "/Users/andreas/micromamba/etc/profile.d/micromamba.sh"
-    else
-        export  PATH="/Users/andreas/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
-    fi
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
 fi
 unset __mamba_setup
+# <<< mamba initialize <<<
 
 
 # === MAIN ===
