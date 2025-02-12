d1e61f2b7c76a91bba67b772f0c9cbe3bc28d056
Andreas Weder <a.weder@docuteam.ch>
2025-02-12 15:48:17 +0100

    Am properly initializing the current shell now with defs required by micromamba.

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -10,18 +10,8 @@ NOTEBOOK_DIR="$HOME/Documents/Projekte/Objekterkennung.$CONDA_ENV/notebooks"
 
 # Note that we currently DO NOT initialize homebrew here as fallback after conda as well.
 
-# --- Initialize micromamba
-# 	  The following code is copied from "micromamba shell init"
-export MAMBA_EXE='/opt/homebrew/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/Users/andreas/micromamba';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
-fi
-unset __mamba_setup
-# <<< mamba initialize <<<
+# Initialize micromamba
+eval "$(micromamba shell hook --shell zsh)"
 
 
 # === MAIN ===
