e7fc099ea809a0ad02dba84df6917ee0a185da16
Andreas Weder <andreas@weder.net>
2025-04-13 09:20:33 +0200

    Move logging initialisation to "config" file. Adds re-initialisation of root logger, if we detect it's required, to avoid that our configuration is ignored.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 HOME_DIR = os.getcwd()
@@ -7,3 +8,22 @@ OUTPUT_DIR = os.path.join(HOME_DIR, "output/predict")
 
 # Supported languages
 SUPPORTED_LANGUAGES = ["de"]
+
+# Initialise the logging framework
+#
+#   The log level can be controlled via the LOG_LEVEL environment variable, and the log file
+#   name can be set using the LOG_FILE environment variable. By default, the log level is set
+#   to INFO, and logs are written to 'app.log'.
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+log_file = os.getenv("LOG_FILE", "app.log")
+
+# Reset the root logger only if it already has handlers
+if logging.root.handlers:
+    print("Re-initialising the root logger. To avoid this, try to import the config module right after the system modules.")
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
