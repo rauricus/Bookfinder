fa1231f89ec65862c21a1a6686a5d64ce743505c
Andreas Weder <andreas@weder.net>
2026-05-30 22:16:24 +0200

    Moves existing training results and models from OBB training to separate "obb" subfolders in a preparation for "aabb" variants.

--- train/obb/train_obb.sh
+++ train/obb/train_obb.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env zsh
 
-# Trains a YOLO26 OBB model and resumes automatically after crashes.
+# Trains a YOLO26n OBB model (Pi 5) and resumes automatically after crashes.
 # Requires the bookfinder environment to be active.
 #
-# Usage: ./train/train.sh [name] [model] [epochs]
-#   name    Training run name (default: train4)
+# Usage: ./train/obb/train_obb.sh [name] [model] [epochs]
+#   name    Training run name (default: train5)
 #   model   Base model file   (default: yolo26n-obb.pt)
 #   epochs  Number of epochs  (default: 200)
 #
 # Examples:
-#   ./train/train.sh                               # train4 with yolo26n-obb.pt, 200 epochs
-#   ./train/train.sh train4 yolo26n-obb.pt 100    # train4 with 100 epochs
-#   ./train/train.sh train5 yolo26n-obb.pt 200    # train5
+#   ./train/obb/train_obb.sh                            # train5, 200 epochs
+#   ./train/obb/train_obb.sh train5 yolo26n-obb.pt 200
 
-cd "${0:A:h}/.."  # always run relative to project root
+cd "${0:A:h}/../.."  # always run relative to project root
 
-NAME="${1:-train4}"
+NAME="${1:-train5}"
 MODEL="${2:-yolo26n-obb.pt}"
 EPOCHS="${3:-200}"
 
@@ -33,9 +32,10 @@ while true; do
   else
     yolo task=obb mode=train \
       model="$MODEL" \
-      "data=datasets/Book spine detection.v2.yolo8-obb/data.yaml" \
-      device=mps epochs=$EPOCHS imgsz=320 batch=16 patience=100 \
-      lr0=0.005 \
+      "data=datasets/Book spine detection P2.obb/data.yaml" \
+      device=mps epochs=$EPOCHS imgsz=640 batch=8 patience=50 \
+      lr0=0.01 \
+      degrees=15 shear=5 perspective=0.001 hsv_v=0.4 blur=0.01 \
       name="$NAME"
   fi
   [[ $? -eq 0 ]] && break
